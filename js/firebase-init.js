968a0641a7334d3de1a40467d2f94d7837040a72
cbjworld <113494404+cbjworld@users.noreply.github.com>
2026-08-12 13:45:39 +0900

    Update Firebase configuration with new credentials

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,12 +1,13 @@
 /* ============ Firebase 초기화 ============ */
 // 1) Firebase 콘솔 > 프로젝트 설정 > 내 앱 에서 나온 firebaseConfig 값을 아래에 그대로 붙여넣으세요.
 const firebaseConfig = {
-  apiKey: "여기에_API_KEY",
-  authDomain: "여기에_프로젝트ID.firebaseapp.com",
-  projectId: "여기에_프로젝트ID",
-  storageBucket: "여기에_프로젝트ID.appspot.com",
-  messagingSenderId: "여기에_숫자",
-  appId: "여기에_앱ID"
+  apiKey: "AIzaSyDKqXtLp5GoOmC7GjRO1P2Gd23qRHMJBB0",
+  authDomain: "babjang-af788.firebaseapp.com",
+  projectId: "babjang-af788",
+  storageBucket: "babjang-af788.firebasestorage.app",
+  messagingSenderId: "864764163788",
+  appId: "1:864764163788:web:c677d2648270b27c637469",
+  measurementId: "G-T8HMGR9SZ9"
 };
 
 firebase.initializeApp(firebaseConfig);
